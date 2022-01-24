c203d4283ea40e2b472d1e0ace349ce11520930f
Nicole George <hello@nicolegeorge.me>
2022-01-24 11:22:58 +1030

    added edit function

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -40,6 +40,14 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
+  // update feedback items
+
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+
   // set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
@@ -52,10 +60,11 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit
+        updateFeedback
       }}
     >
       {children}
